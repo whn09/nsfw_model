c8246492da373437c1f60a26f90baf9c1889d284
henan.wang <henan.wang@henan.wang's MacBook Pro>
2019-03-06 19:47:26 +0800

    add batch_size

--- test.py
+++ test.py
@@ -8,13 +8,16 @@ print('result:', result)
 import os
 base_dir = '../dataset/video2/images/'
 filenames = os.listdir(base_dir)
+for i in range(len(filenames)):
+    filenames[i] = base_dir+filenames[i]
 fout = open('../dataset/video2/haha_video_keras_nsfw_score.csv', 'w')
 fout.write('content_id,nsfw_score\n')
-for i,filename in enumerate(filenames):
-    if i % 1000 == 0:
-        print(i, '/', len(filenames))
-    result = detector.predict(base_dir+filename)
-    for k, v in result.items():  # only one element
+batch_size = 1024
+for i in range(len(filenames)/batch_size+1):
+    print(i*batch_size, '/', len(filenames))
+    result = detector.predict(filenames[i*batch_size:(i+1)*batch_size], batch_size=batch_size)
+    for k, v in result.items():
+        content_id = k.replace(base_dir, '')[:-4]
         nsfw_score = v['sexy']+v['porn']+v['hentai']
-        fout.write(filename[:-4]+','+str(nsfw_score)+'\n')
+        fout.write(content_id+','+str(nsfw_score)+'\n')
 fout.close()
